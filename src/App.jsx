30aa8807377bb920fdf318c48a628c3735a46525
TuanKhoa88 <tuankhoakt@gmail.com>
2023-06-30 17:10:06 +0700

    add router-dom, lazy load, craco, alias

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import './App.scss'
+import LazyLoad from './LazyLoad'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      1122334
+      <div className="app_container">
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="" element={LazyLoad(() => import("@pages/Homes/Home"))()} />
+          <Route path="/about" element={LazyLoad(() => import("@pages/Abouts/About"))()} />
+          <Route path="/about" element={LazyLoad(() => import("@pages/Abouts/About"))()} >
+            {/* Route con của About */}
+            <Route path="my-infor" element={LazyLoad(() => import("@pages/Abouts/MyInfors/MyInfor"))()} />/
+          </Route>
+
+        </Routes>
+      </div>
     </div>
   );
 }
